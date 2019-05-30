f38797254109c3d1112eab01d8d3338985afcb22
Molly Perlich <43156069+mollyperlich@users.noreply.github.com>
2019-05-29 22:49:30 -0700

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,150 @@
+// pull data from all significant earthquakes in the past week
+var jsonurl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
+
+d3.json(jsonurl, function(data) {
+  // send the data.features object to the cr_Features function.
+  console.log(data)
+  features(data.features);
+});
+
+// Create features from earthquake data
+function createFeatures(earthquakeData) {
+
+  // Make popups defining the place and time of the earthquake
+  function eachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features on the earthquakeData object
+  // Run the onEachFeature function 
+  var earthquakes = L.geoJSON(earthquakeData, {
+    pointToLayer: function (feature, latlng) {
+              return L.circleMarker(latlng, {
+        radius: markerSize(feature.properties.mag),
+        fillColor: fillColor(feature.properties.mag),
+        color: "black",
+        weight: 0.6,
+        opacity: 0.4,
+        fillOpacity: 0.6
+      });
+      },
+
+      // Create popups
+      eachFeature: function (feature, layer) {
+        return layer.bindPopup(`<strong>Place:</strong> ${feature.properties.place}<br><strong>Magnitude:</strong> ${feature.properties.mag}`);
+      }
+    });
+
+    // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+
+//creting  map using two layers.
+function createMap(earthquakes) {
+
+  // Define streetmap and darkmap layers
+  var streetMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
+
+  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.light",
+    accessToken: API_KEY
+  });
+  var baseMaps = {
+    "Street Map": streetMap,
+    "Light Map": lightMap
+  };
+
+   // Create overlay object to hold our overlay layer
+   var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+    // Create our map, giving it the streetmap and earthquakes layers to display on load.
+  // map center is Trenton, NJ.
+  var myMap = L.map("map", {
+    center: [
+      40.22, -74.75
+    ],
+    zoom: 3,
+    layers: [streetmap, earthquakes]
+  });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+
+// Set up the legend and the colour variance for magnitude from leaflet documentation
+var legend = L.control({ position: 'bottomright'});
+
+
+  legend.onAdd = function() {
+    var div = L.DomUtil.create('div', 'info legend'),
+        magnitude = [0,1,2,3,4,5,6],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < magnitude.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + fillColor(magnitude[i] + 1) + '"></i> ' +
+            magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
+
+  };
+
+// Define colors depending on the magnituge of the earthquake
+function fillColor(magnituge) {
+
+  switch (true) {
+    case magnituge >= 6.0:
+      return 'red';
+      break;
+    
+    case magnituge >= 5.0:
+      return 'orangered';
+      break;
+
+    case magnituge >= 4.0:
+      return 'darkorange';
+      break;
+    
+    case magnituge >= 3.0:
+      return 'orange';
+      break;
+
+    case magnituge >= 2.0:
+      return 'gold';
+      break;
+
+    case magnituge >= 1.0:
+      return 'yellow';
+      break;
+
+    default:
+      return 'greenyellow';
+  };
+};
+
+
+// Reflect the earthquake magnitude
+function markerSize(magnituge) {
+  return magnituge*3;
+}
+
